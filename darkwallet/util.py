ef0450ffc3ca7a952f4cb20b644286f280c7eb14
Yarvis Hatsa <PoliceTerror@dyne.org>
2017-01-17 18:04:28 +0000

    find path to copy config file when not in the same dir

--- darkwallet/util.py
+++ darkwallet/util.py
@@ -1,6 +1,7 @@
 import errno
 import os
 import shutil
+import sys
 
 def make_sure_dir_exists(path):
     try:
@@ -9,10 +10,13 @@ def make_sure_dir_exists(path):
         if exception.errno != errno.EEXIST:
             raise
 
+def _config_template():
+    return os.path.join(sys.path[0], "darkwallet.cfg")
+
 def make_sure_file_exists(filename):
     if not os.path.isfile(filename):
         print("Initializing new darkwallet.cfg.")
-        shutil.copyfile("darkwallet.cfg", filename)
+        shutil.copyfile(_config_template(), filename)
 
 def list_files(path):
     return [filename for filename in os.listdir(path)
